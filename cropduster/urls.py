d09fa7baecf0f09fdb77d134898fdf5fd6bbb106
Benjy Malca <bnmalcabis@gmail.com>
2017-09-04 12:06:43 -0500

    Changed format of urlpatterns

--- cropduster/urls.py
+++ cropduster/urls.py
@@ -4,14 +4,16 @@
 import os
 
 from django.conf.urls import url
+from django.views.static import serve
 
+from cropduster import views
 
 urlpatterns = [
     url(
-        r'^_static/(?P<path>.*)$', "django.views.static.serve",
+        r'^_static/(?P<path>.*)$', serve,
         {"document_root": os.path.dirname(__file__) + "/media"},
         name='cropduster-static'
     ),
-    url(r'^upload/$', "cropduster.views.upload", name='cropduster-upload'),
-    url(r'^ratio/$', "cropduster.views.get_ratio", name='cropduster-ratio'),
+    url(r'^upload/$', views.upload, name='cropduster-upload'),
+    url(r'^ratio/$', views.get_ratio, name='cropduster-ratio'),
 ]
